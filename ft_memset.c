077c2882d0f11ce6436dd8c7e124e3a1f7d6dfa8
Akram Sallam <akramsallam111@gmail.com>
2026-09-25 01:02:25 +0300

    template of functions

---
+++ ft_memset.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void	*memset(void *s, int c, size_t n)
+{
+	unsigned char	*p;
+	unsigned char	val;
+
+	p = (unsigned char *)s;
+	val = (unsigned char)c;
+	while (n--)
+	{
+		*p = val;
+		p++;
+	}
+	return (s);
+}

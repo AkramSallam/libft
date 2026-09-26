3016a54a4fcd6e046fc6072c4ca19e0186498f9c
Akram Sallam <akramsallam111@gmail.com>
2026-09-26 17:30:51 +0300

    new implementations

--- ft_memmove.c
+++ ft_memmove.c
@@ -2,5 +2,43 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	unsigned char	*d;
+	const unsigned char	*s;
 
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	if (s == d || n == 0)
+		return (dest);
+	if (s < d)
+	{
+		d += n;
+		s += n;
+		while (n--)
+		{
+			*d = *s;
+			d--;
+			s--;
+		}
+	}
+	else
+	{
+		while (n--)
+		{
+			*d = *s;
+			d++;
+			s++;
+		}
+	}
+	return (dest);
 }
+/*
+#include <stdio.h>
+
+int main()
+{
+	char s[] = "123456";
+	ft_memmove(s+2, s, 4);
+	printf("%s\n", s);
+	return 0;
+}
+*/
--- ft_strlcat.c
+++ ft_strlcat.c
@@ -2,5 +2,35 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	
+	size_t	i;
+	size_t	j;
+	size_t	dstlen;
+	size_t	srclen;
+
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	if (dstsize == 0)
+		return (srclen);
+	if (dstsize <= dstlen)
+		return (srclen + dstsize);
+	j = 0;
+	while (dst[j])
+		j++;
+	i = 0;
+	while (src[i] && j < dstsize - 1)
+	{
+		dst[j] = src[i];
+		i++;
+		j++;
+	}
+	dst[j] = '\0';
+	return (dstlen + srclen);
+}
+#include <stdio.h>
+
+int main(){
+	char dst[5] = "hello";
+	char src[] = "!!!";
+	printf("%d\n", ft_strlcat(dst, src, 5));
+	return 0;
 }
